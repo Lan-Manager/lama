0b6859630b2d36a3ac74a2f1d5618da45cf25595
maxeber <maxhebert13@gmail.com>
2016-10-16 20:20:25 -0400

    Enlever les prix de participation

--- Lama/Logic/Model/Test/Tournoi.cs
+++ Lama/Logic/Model/Test/Tournoi.cs
@@ -11,8 +11,9 @@ namespace Lama.Logic.Model.Test
     {
         public string Nom { get; set; }
         public string Etat { get { return "Tournoi en cours"; } }
-        public ObservableCollection<Tour> Tours { get { return new ObservableCollection<Tour> { new Tour(), new Tour(), new Tour(), new Tour() }; } set { } }
         public Equipe Vainqueur { get { return new Equipe(2); } set { } }
+        public ObservableCollection<Tour> Tours { get { return new ObservableCollection<Tour> { new Tour(), new Tour(), new Tour(), new Tour() }; } set { } }
+        public ObservableCollection<Prix> lstPrix { get { return new ObservableCollection<Prix> { new Prix("25$ carte cadeau à la cafétéria"), new Prix() }; } set { } }
 
         public Tournoi(string nom)
         {
