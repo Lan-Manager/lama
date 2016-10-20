9562ff282aeef4f173eef231fca0118a7e0c7bd6
Antoine Richer <Antoine Richer>
2016-10-20 15:24:00 -0400

    Ajout des classes après tentative de fix un merge conflict

---
+++ Lama/Logic/Poste.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lama.Logic.Model
+{
+    public class Poste : INotifyPropertyChanged
+    {
+        public int Numero { get; set; }
+        public ObservableCollection<string> LstEtatPossible { get; set; }
+        private string _etat;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Etat
+        {
+            get
+            {
+                return _etat;
+            }
+            set
+            {
+                if (value != _etat)
+                {
+                    _etat = value;
+                    NotifyPropertyChanged("Etat");
+                }
+            }
+        }
+        public void NotifyPropertyChanged(string nomProp)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nomProp));
+
+        }
+
+        public Volontaire DerniereModificationPar { get; set; }
+        public Poste(int numero, string etat)
+        {
+            LstEtatPossible = new ObservableCollection<string>();
+            LstEtatPossible.Add("En attente");
+            LstEtatPossible.Add("Problème");
+            LstEtatPossible.Add("Prêt");
+            Numero = numero;
+            Etat = etat;
+        }
+
+
+
+    }
+}
