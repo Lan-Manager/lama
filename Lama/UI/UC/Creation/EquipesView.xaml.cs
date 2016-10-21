7ddaa9e2f372627db7d860f78bc553983483a0e3
1348151 <1348151@LD125-17-03980>
2016-10-21 14:33:16 -0400

    Enlevé des champs

--- Lama/UI/UC/Creation/EquipesView.xaml.cs
+++ Lama/UI/UC/Creation/EquipesView.xaml.cs
@@ -9,21 +9,9 @@ namespace Lama.UI.UC.Creation
     /// </summary>
     public partial class EquipesView : UserControl
     {
-        ObservableCollection<string> lstEquipes;
-
         public EquipesView()
         {
             InitializeComponent();
-
-            lstEquipes = new ObservableCollection<string>();
-            
-            dgEquipes.ItemsSource = lstEquipes;
-        }
-
-        private void btnAddEquipe_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            lstEquipes.Add(txtNewEquipe.Text);
-            txtNewEquipe.Clear();
         }
     }
 }
--- Lama/UI/UC/Creation/ParticipantsView.xaml.cs
+++ Lama/UI/UC/Creation/ParticipantsView.xaml.cs
@@ -16,40 +16,5 @@ namespace Lama.UI.UC.Creation
         {
             InitializeComponent();
         }
-
-        private void btnImport_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            // Permettra d'ouvrir l'explorateur windows
-            OpenFileDialog ofd = new OpenFileDialog();
-
-            // Settings pour ofd
-            ofd.Filter = "CSV files (*.csv)|*.csv";
-            ofd.DefaultExt= ".csv";
-            
-            // Ouvrir l'explorateur
-            ofd.ShowDialog();
-
-            // Ouvrir le fichier
-            StreamReader sr = new StreamReader(ofd.OpenFile());
-
-
-            ////////////////////////////////////////////////////////////////////////
-
-            int i = 0;
-
-             
-            while (!sr.EndOfStream)
-            {
-                i++;
-                MessageBox.Show(i + " " + sr.ReadLine());
-            }
-
-
-            ////////////////////////////////////////////////////////////////////////
-
-
-            // Fermer le stream
-            sr.Close();
-        }
     }
 }
