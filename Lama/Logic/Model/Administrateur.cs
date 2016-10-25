f9ddd92be5900f2a683b3e96716b7bce3e646161
Antoine Richer <Antoine Richer>
2016-10-24 22:01:25 -0400

    Ajout classe Administrateur

---
+++ Lama/Logic/Model/Administrateur.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lama.Logic.Model
+{
+    public class Administrateur : Volontaire
+    {
+        // Constructeur par défaut.
+        public Administrateur(string nom, string prenom, string courriel, string nomUtilisateur) : base(nom, prenom, courriel, nomUtilisateur)
+        {
+            
+        }
+    }
+}
