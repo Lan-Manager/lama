463ea77915807733e9cedfbcf685a4c6aef9f69b
maxeber <maxhebert13@gmail.com>
2016-11-04 15:17:42 -0400

    Améliorer l'ui de la fenêtre de la grille du tournoi

--- Lama/UI/UC/TournoiControls/GrilleControls/ToursUC.xaml.cs
+++ Lama/UI/UC/TournoiControls/GrilleControls/ToursUC.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Lama.UI.UC.TournoiControls.GrilleControls
 {
@@ -20,16 +22,38 @@ namespace Lama.UI.UC.TournoiControls.GrilleControls
     /// </summary>
     public partial class ToursUC : UserControl
     {
+        private Boolean AutoScroll = true;
+        private Boolean DefaultScrollEnd = true;
+
         public ToursUC()
         {
             InitializeComponent();
-        }       
+            svTours.ScrollToBottom();  
+        }
 
         private void ScrollViewer_ScrollChanged(Object sender, ScrollChangedEventArgs e)
         {
-            if (((ScrollViewer)sender).VerticalOffset == ((ScrollViewer)sender).ScrollableHeight)
-            {
-                ((ScrollViewer)sender).ScrollToEnd();
+            // User scroll event : set or unset autoscroll mode
+            if (e.ExtentHeightChange == 0)
+            {   // Content unchanged : user scroll event
+                if (svTours.VerticalOffset == svTours.ScrollableHeight)
+                {   // Scroll bar is in bottom
+                    // Set autoscroll mode
+                    AutoScroll = true;
+                }
+                else
+                {   // Scroll bar isn't in bottom
+                    // Unset autoscroll mode
+                    AutoScroll = false;
+                }
+            }
+
+            // Content scroll event : autoscroll eventually
+            if (AutoScroll && e.ExtentHeightChange != 0)
+            {   // Content changed and autoscroll mode set
+                // Autoscroll
+                svTours.ScrollToBottom();
+                svTours.ScrollToEnd();
             }
         }
     }
