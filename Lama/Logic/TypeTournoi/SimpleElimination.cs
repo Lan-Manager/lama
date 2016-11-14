e15463064c1ca2d77679ad9bdc1a79c3f887e241
Tristan <kraig.tri125@gmail.com>
2016-11-14 09:46:52 -0500

    Début de la programmation de l'interface ITypeTournoi pour le support de différent types de tournois.

---
+++ Lama/Logic/TypeTournoi/SimpleElimination.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lama.Logic.TypeTournoi
+{
+    public class SimpleElimination : ITypeTournoi
+    {
+        public void GenererTour()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
