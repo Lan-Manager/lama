81473036d8c453fad271eb61d33b215cb32ec9b3
Tristan <kraig.tri125@gmail.com>
2016-11-24 23:24:50 -0500

    PrixHelper avec méthode pour récupérer les prix du tournoi actif avec les équipes gagnantes s'il y a lieu.

---
+++ LamaBD/helper/PrixHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamaBD.helper
+{
+    public class PrixHelper
+    {
+        public static async Task<List<prix>> SelectAllPrixTournoiAsync()
+        {
+            using (var ctx = new Connexion420())
+            {
+                var task = TournoiHelper.SelectLast();
+                int idTournoi = task.Result.idTournoi;
+
+                var query = ctx.prix
+                    .Include(x => x.prixtournois)
+                    .Include(x => x.equipes)
+                    .Where(x => x.prixtournois.Any(y => y.idTournoi == idTournoi))
+                    .OrderBy(x => x.nom);
+
+                return await query.ToListAsync();
+            }
+        }
+    }
+}
