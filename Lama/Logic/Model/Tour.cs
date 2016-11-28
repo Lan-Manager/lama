be564ebd146ea44dbeed5d5b41314ee9c30be634
Tristan <kraig.tri125@gmail.com>
2016-11-28 16:38:25 -0500

    Création des Tours, Parties et EquipesParties dans la base de données.

--- Lama/Logic/Model/Tour.cs
+++ Lama/Logic/Model/Tour.cs
@@ -10,14 +10,52 @@ namespace Lama.Logic.Model
     public class Tour
     {
         public string Nom { get; set; }
+        public int NumeroTour { get; set; }
         public ObservableCollection<Partie> LstParties { get; set; }
 
         public Tour(int numTour)
         {
+            NumeroTour = numTour;
             StringBuilder sb = new StringBuilder("Tour ").Append(numTour);
 
             Nom = sb.ToString();
             LstParties = new ObservableCollection<Partie>();
+        }
+
+
+        public bool Insert(LamaBD.tournois tournois)
+        {
+            LamaBD.tours entity = new LamaBD.tours();
+            entity.idTournoi = tournois.idTournoi;
+            entity.dateDebut = DateTime.Now;
+            entity.numTour = NumeroTour;
+            entity.dateFin = null;
+            foreach (Partie partie in LstParties)
+            {
+                LamaBD.parties pEntity = new LamaBD.parties();
+                pEntity.tours = entity;
+                pEntity.numPartie = partie.NumeroPartie;
+                foreach (PartieEquipe pE in partie.LstEquipes)
+                {
+                    LamaBD.equipesparties entityPartieEquipe = new LamaBD.equipesparties();
+                    entityPartieEquipe.parties = pEntity;
+                    entityPartieEquipe.estGagnante = null;
+                    var taskEquipe = LamaBD.helper.EquipeHelper.Select(pE.Equipe.Nom);
+                    taskEquipe.Wait();
+                    int idEquipe = taskEquipe.Result.idEquipe;
+                    entityPartieEquipe.idEquipe = idEquipe;
+
+                    pEntity.equipesparties.Add(entityPartieEquipe);
+                }
+                entity.parties.Add(pEntity);
+            }
+
+            var task = LamaBD.helper.TourHelper.InsertAsync(entity);
+            task.Wait();
+            if (task.Result)
+                return true;
+            else
+                return false;
         }
     }
 }
--- LamaBD/helper/TourHelper.cs
+++ LamaBD/helper/TourHelper.cs
@@ -42,5 +42,25 @@ namespace LamaBD.helper
                 return await query.ToListAsync();
             }
         }
+
+        public static async Task<bool> InsertAsync(tours obj)
+        {
+            using (var ctx = new Connexion420())
+            {
+                ctx.tours.Add(obj);
+
+                try
+                {
+                    await ctx.SaveChangesAsync();
+                }
+                catch (Exception exct)
+                {
+                    Console.WriteLine(exct.Message);
+                    return false;
+                }
+
+                return true;
+            }
+        }
     }
 }
